05cd37c05d1e24ade83d1f95da8d0ca4383dfb09
Dama Dany <damadanyna@gmail.com>
2023-03-28 12:33:28 +0300

    mise en place du style du button du sideBar  (style.css et sidebar)

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class=" flex h-full w-full bg-white flex-row">
+<div class=" flex h-full w-full bg-white flex-col px-3">
     <div class="flex flex-row py-5 w-full justify-center">
         <span class=" text-xl font-extrabold">Dossier Formation</span>
     </div>
-  </div>
+    <div class="flex flex-col ">
+        <v-btn prepend-icon="mdi-home" variant="tonal " class=" bg-black flex justify-start" >  </v-btn>
+    </div>
+</div>
 </template>
 
 <script>
@@ -12,6 +15,5 @@ export default {
 }
 </script>
 
-<style>
-
+<style> 
 </style>
