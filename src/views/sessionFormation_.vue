4739c9c08e8be63ac004246b00182035615090fa
Dama Dany <damadanyna@gmail.com>
2023-03-29 21:51:29 +0300

    confirmation du designe de la page session Formation

--- src/views/sessionFormation_.vue
+++ src/views/sessionFormation_.vue
@@ -1,84 +1,50 @@
 <template>
 <div class="flex flex-col h-full">
     <h1 class=" text-lg sticky -top-6 w-full bg-stone-100 z-50 font-bold text-stone-600 pt-2 pb-9">Session Formation</h1>
-    <div class=" d bg-white py-4 rounded-lg w-full h-max px-12 flex z-10 flex-col ">
-        <div class="flex flex-row">
-            <div class="flex ml-12 flex-row items-center">
-                <svg class=" text-stone-500 w-5 fill-current" viewBox="0 0 24 24">
-                    <path d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5-1.5 1.5-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16 6.5 6.5 0 0 1 3 9.5 6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14 14 12 14 9.5 12 5 9.5 5z" /></svg>
-                <input type="text" class=" px-3 border-b border-stone-400 outline-none focus:border-black " placeholder=" Lancer un recherche ">
-            </div>
+    <div class=" d bg-white h-full py-4 rounded-lg w-full px-12 flex z-10 flex-col ">
+        <div v-if="listeFormatoin.length>0" class="flex-row">
+            <div class="flex flex-row">
+                <div class="flex ml-12 flex-row items-center">
+                    <svg class=" text-stone-500 w-5 fill-current" viewBox="0 0 24 24">
+                        <path d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5-1.5 1.5-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16 6.5 6.5 0 0 1 3 9.5 6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14 14 12 14 9.5 12 5 9.5 5z" /></svg>
+                    <input type="text" class=" px-3 border-b border-stone-400 outline-none focus:border-black " placeholder=" Lancer un recherche ">
+                </div>
 
-            <div class="flex w-full justify-end relative">
-                <btn_Vue class=" " :options="{label:'editer',style:' bg-stone-700 text-white w-full',ico:$store.state.icons.edit}"></btn_Vue>
+                <div class="flex w-full justify-end relative">
+                    <btn_Vue class=" " :options="{label:'editer',style:' bg-stone-700 text-white w-full',ico:$store.state.icons.edit}"></btn_Vue>
+                </div>
+            </div>
+            <div class="flex flex-col mt-5">
+                <div v-for=" item,i in listeFormatoin" class="flex duration-300 flex-col hover:bg-black group py-3 my-2 border-[1px] px-4 rounded-md border-black ">
+                    <div class="flex flex-row justify-between w-full bg-stone-">
+                        <span class=" text-stone-700 font-semibold group-hover:text-white" v-text="item.titre"></span>
+                        <span class=" text-stone-400">#001</span>
+                    </div>
+                    <div class="flex flex-row justify-between">
+                        <div class=" flex flex-row ">
+                            <div class=" flex flex-row mx-1 ">
+                                <btn_Vue class=" " :options="{label:item.label_btn1,style:' bg-stone-600 py-1 text-stone-100 '}"></btn_Vue>
+                            </div>
+                            <div class=" flex flex-row mx-1 ">
+                                <btn_Vue class=" " :options="{label:item.label_btn2,style:' bg-fuchsia-700 py-1 text-stone-100 '}"></btn_Vue>
+                            </div>
+                            <div class=" flex flex-row mx-1  ">
+                                <btn_Vue class=" " :options="{label:item.label_btn3,style:' bg-stone-100 py-1 text-stone-800 '}"></btn_Vue>
+                            </div>
+                        </div>
+                        <div class=" flex flex-row ">
+                            <btn_Vue class=" " :options="{label:item.label_btn4,style:' bg-stone-200 py-1 text-red ',ico:$store.state.icons.edit}"></btn_Vue>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="flex flex-col mt-5">
-            <div class="flex duration-300 flex-col hover:bg-black group py-3 my-2 border-[1px] px-4 rounded-md border-black ">
-                <div class="flex flex-row justify-between w-full bg-stone-">
-                    <span class=" text-stone-700 font-semibold group-hover:text-white">Titre formation</span>
-                    <span class=" text-stone-400">#001</span>
-                </div>
-                <div class="flex flex-row justify-between">
-                    <div class=" flex flex-row "> 
-                        <div class=" flex flex-row mx-1 ">
-                            <btn_Vue class=" " :options="{label:'Gestion bénéficiaire',style:' bg-stone-600 py-1 text-stone-100 '}"></btn_Vue>
-                        </div> 
-                        <div class=" flex flex-row mx-1 ">
-                            <btn_Vue class=" " :options="{label:'Gestion session',style:' bg-fuchsia-700 py-1 text-stone-100 '}"></btn_Vue>
-                        </div> 
-                        <div class=" flex flex-row mx-1  ">
-                            <btn_Vue class=" " :options="{label:'Gestion Formation',style:' bg-stone-100 py-1 text-stone-800 '}"></btn_Vue>
-                        </div> 
-                    </div>
-                    <div class=" flex flex-row ">
-                        <btn_Vue class=" " :options="{label:'editer',style:' bg-stone-200 py-1 text-red ',ico:$store.state.icons.edit}"></btn_Vue>
-                    </div>
-                </div>
-            </div>
-            <div class="flex duration-300 flex-col hover:bg-black group py-3 my-2 border-[1px] px-4 rounded-md border-black ">
-                <div class="flex flex-row justify-between w-full bg-stone-">
-                    <span class=" text-stone-700 font-semibold group-hover:text-white">Titre formation</span>
-                    <span class=" text-stone-400">#001</span>
-                </div>
-                <div class="flex flex-row justify-between">
-                    <div class=" flex flex-row "> 
-                        <div class=" flex flex-row mx-1 ">
-                            <btn_Vue class=" " :options="{label:'Gestion bénéficiaire',style:' bg-stone-600 py-1 text-stone-100 '}"></btn_Vue>
-                        </div> 
-                        <div class=" flex flex-row mx-1 ">
-                            <btn_Vue class=" " :options="{label:'Gestion session',style:' bg-fuchsia-700 py-1 text-stone-100 '}"></btn_Vue>
-                        </div> 
-                        <div class=" flex flex-row mx-1  ">
-                            <btn_Vue class=" " :options="{label:'Gestion Formation',style:' bg-stone-100 py-1 text-stone-800 '}"></btn_Vue>
-                        </div> 
-                    </div>
-                    <div class=" flex flex-row ">
-                        <btn_Vue class=" " :options="{label:'editer',style:' bg-stone-200 py-1 text-red ',ico:$store.state.icons.edit}"></btn_Vue>
-                    </div>
-                </div>
-            </div>
-            <div class="flex duration-300 flex-col hover:bg-black group py-3 my-2 border-[1px] px-4 rounded-md border-black ">
-                <div class="flex flex-row justify-between w-full bg-stone-">
-                    <span class=" text-stone-700 font-semibold group-hover:text-white">Titre formation</span>
-                    <span class=" text-stone-400">#001</span>
-                </div>
-                <div class="flex flex-row justify-between">
-                    <div class=" flex flex-row "> 
-                        <div class=" flex flex-row mx-1 ">
-                            <btn_Vue class=" " :options="{label:'Gestion bénéficiaire',style:' bg-stone-600 py-1 text-stone-100 '}"></btn_Vue>
-                        </div> 
-                        <div class=" flex flex-row mx-1 ">
-                            <btn_Vue class=" " :options="{label:'Gestion session',style:' bg-fuchsia-700 py-1 text-stone-100 '}"></btn_Vue>
-                        </div> 
-                        <div class=" flex flex-row mx-1  ">
-                            <btn_Vue class=" " :options="{label:'Gestion Formation',style:' bg-stone-100 py-1 text-stone-800 '}"></btn_Vue>
-                        </div> 
-                    </div>
-                    <div class=" flex flex-row ">
-                        <btn_Vue class=" " :options="{label:'editer',style:' bg-stone-200 py-1 text-red ',ico:$store.state.icons.edit}"></btn_Vue>
-                    </div>
-                </div>
+        <div v-else class=" text-stone-500 flex-col w-full h-full items-center justify-center flex">
+            <img src="../assets/vide.svg" alt="">
+            <span>Vous avez aucun formation.</span>
+            <span>Cliquer ici pour creer.</span>
+            <div class=" flex flex-row mx-1  ">
+                <btn_Vue class=" " :options="{label:'Nouvelle',ico:$store.state.icons.plus,style:' bg-stone-900 text-white py-2 text-stone-800 '}"></btn_Vue>
             </div>
         </div>
     </div>
@@ -94,9 +60,33 @@ export default {
     },
     data() {
         return {
-            liste_page:{
-                
-            }
+            listeFormatoin: [
+                //     {
+                //     titre: 'Titre formation',
+                //     label_btn1: 'Gestion bénéficiaire',
+                //     label_btn2: 'Gestion session',
+                //     label_btn3: 'Gestion Formation',
+                //     label_btn4: 'Supprimer',
+                // }, {
+                //     titre: 'Titre formation',
+                //     label_btn1: 'Gestion bénéficiaire',
+                //     label_btn2: 'Gestion session',
+                //     label_btn3: 'Gestion Formation',
+                //     label_btn4: 'Supprimer',
+                // }, {
+                //     titre: 'Titre formation',
+                //     label_btn1: 'Gestion bénéficiaire',
+                //     label_btn2: 'Gestion session',
+                //     label_btn3: 'Gestion Formation',
+                //     label_btn4: 'Supprimer',
+                // }, {
+                //     titre: 'Titre formation',
+                //     label_btn1: 'Gestion bénéficiaire',
+                //     label_btn2: 'Gestion session',
+                //     label_btn3: 'Gestion Formation',
+                //     label_btn4: 'Supprimer',
+                // },
+            ]
         }
     }
 
