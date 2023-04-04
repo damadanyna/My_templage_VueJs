0b778463bcd6ae1d8388133f432970520596fa67
Dama Dany <damadanyna@gmail.com>
2023-04-04 10:23:54 +0300

    confirmation de l'ajout du composant actionFormation

---
+++ src/components/poppup/actionFormation.vue
@@ -0,0 +1,149 @@
+<template>
+<div id="poppupOrganisme">
+    <!-- forumlaire de organisme -->
+    <div class=" bg-white rounded-lg flex px-6 py-5 flex-col z-20">
+        <div class=" mt-8 flex flex-col">
+
+            <div @click="()=>{isChecked==false?isChecked=true:isChecked=false}" class=" items-center text-teal-500 flex flex-row text-xs mb-2">
+                <svg v-if="isChecked==false" class=" fill-current w-5" viewBox="0 0 24 24">
+                    <path d="M19 3H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2m0 2v14H5V5h14z" /></svg>
+                <svg v-else class=" fill-current w-5" viewBox="0 0 24 24">
+                    <path d="m10 17-5-5 1.41-1.42L10 14.17l7.59-7.59L19 8m0-5H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2z" /></svg>
+                <span>Certifiante</span>
+            </div>
+            <div class="flex flex-row w-full">
+                <input_ class=" w-full " :options="data_[0]"></input_>
+                <input_ class=" w-full ml-3" :options="data_[1]"></input_>
+            </div>
+            <div class="flex mt-5 w-full flex-row">
+                <input_ class=" w-full " :options="data_[2]"></input_>
+                <input_ class=" w-full  ml-3" :options="data_[3]"></input_>
+            </div>
+            <span class=" text-teal-500 text-xs mt-2">*Lié à un membre d'équipe</span>
+            <div class="flex mt-5 flex-row w-full">
+                <textArea_ class=" w-full" :options="data_[4]"></textArea_>
+                <textArea_ class=" w-full ml-3" :options="data_[5]"></textArea_>
+            </div>
+            <div class="flex mt-5 w-full flex-row">
+                <input_ class=" w-full " :options="data_[6]"></input_>
+                <input_ class=" w-full  ml-3" :options="data_[7]"></input_>
+            </div>
+            <div class="flex flex-col">
+                <span class=" text-teal-500 text-xs mt-2">*Délais des alertes si aucune activité est recensé </span>
+                <span class=" text-teal-500 text-xs ">sur le compte d'un bénéficiaire</span>
+            </div>
+            <div class="flex mt-5 flex-row w-full">
+                <textArea_ class=" w-full " :options="data_[7]"></textArea_>
+            </div>
+            <div v-if="isChecked==true" class="flex flex-col w-full">
+                <div class="flex flex-col">  <div class="flex mt-5 flex-row w-full">
+                        <textArea_ class=" w-full " :options="data_[9]"></textArea_>
+                    </div> 
+                    <span class=" text-teal-500 text-xs mt-2">*Pour les diplôme de l’éducation national</span>
+                
+                </div>
+            </div>
+            <div class="flex mt-5 flex-row w-full">
+                <textArea_ class=" w-full " :options="data_[8]"></textArea_>
+            </div>
+        </div>
+        <div :class="data_[0].model=='' || data_[1].model=='' || data_[2].model=='' || data_[3].model==''|| data_[4].model || data_[5].model==''?' opacity-50':' opacity-100'" class=" duration-300 flex mt-12 justify-center  flex-row w-full">
+            <btn_ @click="setIt()" :options="{label:'Valider',style:' base_bg text-white',ico:$store.state.icons.done}"></btn_>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import input_ from '../input/inputTxt.vue'
+import textArea_ from '../input/textarea.vue'
+import btn_ from '../button/btn_.vue';
+import gsap from 'gsap'
+export default {
+    data() {
+        return {
+            data_: [{
+                    label: 'Titre',
+                    model: '',
+                    type: 'text'
+                },
+                {
+                    label: 'Durée',
+                    model: '',
+                    type: 'text'
+                },
+                {
+                    label: 'Contacts',
+                    model: '',
+                    type: 'text'
+                },
+                {
+                    label: 'Tarif',
+                    model: '',
+                    type: 'text'
+                },
+                {
+                    label: 'Modalités d’évaluation',
+                    model: '',
+                },
+                {
+                    label: 'Modalités et délais d’accès à la formation',
+                    model: '',
+                },
+                {
+                    label: 'Délais des alertes',
+                    model: '',
+                    type: 'date'
+                },
+                {
+                    label: 'Méthodes mobilisées',
+                    model: '',
+                    type: 'text'
+                },
+                {
+                    label: 'Modalités pédagogiques',
+                    model: '',
+                },
+                {
+                    label: 'Prérequis',
+                    model: '',
+                },
+
+                {
+                    label: 'Modalités de l’etude personalisé',
+                    model: '',
+                },
+
+            ],
+            selected: 'IB',
+            isChecked: false
+        }
+    },
+    components: {
+        input_,
+        textArea_,
+        btn_
+    },
+    methods: {
+        setIt() {
+            console.log(this.data_)
+        },
+        set_(val) {
+            this.selected = val
+        }
+    },
+    mounted() {
+        gsap.fromTo("#poppupOrganisme", {
+            opacity: 0,
+
+        }, {
+            duration: .5,
+            opacity: 1,
+        });
+    }
+}
+</script>
+
+<style>
+    
+    </style>
--- src/views/listeFormation_.vue
+++ src/views/listeFormation_.vue
@@ -1,53 +1,79 @@
 <template>
 <div class="flex flex-col h-full">
     <h1 class=" text-lg sticky -top-6 w-full bg-stone-100 z-50 font-bold text-stone-600 pt-2 pb-9">Liste Formation</h1>
-    <div class=" d bg-white h-full py-4 rounded-lg w-full px-12 flex z-10 flex-col ">
-        <div v-if="listeFormatoin.length>0" class=" text-xs flex-row">
-            <div class="flex flex-row sticky top-10 py-2 bg-white mt-6">
-                <div class="flex ml-12 flex-row items-center">
-                    <svg class=" text-stone-500 w-5 fill-current" viewBox="0 0 24 24">
-                        <path d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5-1.5 1.5-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16 6.5 6.5 0 0 1 3 9.5 6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14 14 12 14 9.5 12 5 9.5 5z" /></svg>
-                    <input type="text" class=" px-3 border-b border-stone-400 outline-none focus:border-black " placeholder=" Lancer un recherche ">
-                </div>
 
-                <div class="flex w-full justify-end relative">
-                    <btn_Vue class=" " :options="{label:'editer',style:' base_bg text-white w-full',ico:$store.state.icons.edit}"></btn_Vue>
-                </div>
-            </div>
-            <div class="flex flex-col mt-5">
-                <div v-for=" item,i in listeFormatoin" class="flex duration-300 flex-col hover:bg-black group py-3 my-2 border-[1px] px-4 rounded-md border-black ">
-                    <div class="flex flex-row justify-between w-full bg-stone-">
-                        <span class=" text-stone-700 font-semibold group-hover:text-white" v-text="item.titre"></span>
-                        <span class=" text-stone-400">#001</span>
-                    </div>
-                    <div class="flex flex-row justify-between">
-                        <div class=" flex flex-row ">
-                            <div class=" flex flex-row mx-1 ">
-                                <btn_Vue class=" " :options="{label:item.label_btn1,style:' bg-stone-600 py-1 text-stone-100 '}"></btn_Vue>
-                            </div>
-                            <div class=" flex flex-row mx-1 ">
-                                <btn_Vue class=" " :options="{label:item.label_btn2,style:' bg-fuchsia-700 py-1 text-stone-100 '}"></btn_Vue>
-                            </div>
-                            <div class=" flex flex-row mx-1  ">
-                                <btn_Vue class=" " :options="{label:item.label_btn3,style:' bg-stone-100 py-1 text-stone-800 '}"></btn_Vue>
-                            </div>
-                        </div>
-                        <div class=" flex flex-row ">
-                            <btn_Vue class=" " :options="{label:item.label_btn4,style:' bg-stone-200 py-1 text-red ',ico:$store.state.icons.edit}"></btn_Vue>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="flex flex-col h-full">
+        <div class=" flex flex-row  sticky duration-300 -top-6 w-full bg-stone-100 z-50 font-bold text-stone-600 pt-2 pb-3">
+            <h1 class=" text-lg">Organismes </h1>
+            <h1 class=" text-lg ml-2  "> > Gestion de site de formation</h1>
         </div>
-        <div v-else class="text-xs  text-stone-500 flex-col w-full h-full items-center justify-center flex">
-            <img src="../assets/vide.svg" alt="">
-            <flex class=" flex items-center my-3 flex-col">
+        <div :class="howOrgPoppup==true?'flex-row':'flex-col'" class="flex  ">
 
-                <span>Vous avez aucun formation.</span>
-                <span>Cliquer ici pour creer.</span>
-            </flex>
-            <div class=" flex flex-row mx-1  ">
-                <btn_Vue class=" " :options="{label:'Nouvelle',ico:$store.state.icons.plus,style:' base_bg text-white py-2 text-stone-800 '}"></btn_Vue>
+            <div :class="howOrgPoppup==true?' px-5':'px-12'" class="flex-col duration-500  bg-white rounded-lg w-full h-max  flex z-10">
+                <div v-if="listeFormatoin.length>0" class=" text-xs flex-row">
+                    <div class="flex flex-row sticky top-10 py-4 bg-white mt-6">
+                        <div class="flex ml-12  flex-row items-center">
+                            <svg class=" text-teal-700 w-5 fill-current" viewBox="0 0 24 24">
+                                <path d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5-1.5 1.5-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16 6.5 6.5 0 0 1 3 9.5 6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14 14 12 14 9.5 12 5 9.5 5z" /></svg>
+                            <input type="text" class=" px-3 border-b border-stone-400 outline-none focus:border-black " placeholder=" Lancer un recherche ">
+                        </div>
+
+                        <div class="flex w-full justify-end relative">
+                            <btn_Vue class=" " :options="{label:'Nouveau',style:' base_bg text-white w-full',ico:$store.state.icons.edit}"></btn_Vue>
+                        </div>
+                    </div>
+                    <div class="flex flex-col mt-5">
+                        <div v-for=" item,i in listeFormatoin" :key="i" class=" group flex duration-300 flex-col hover:bg-[#63B6B9] group py-3 my-2 border-[1px] px-4 rounded-md border-teal-700 ">
+                            <div class="flex flex-row justify-between w-full bg-stone-">
+                                <span class=" text-stone-700 font-semibold group-hover:text-white " v-text="item.titre"></span>
+                                <span class=" text-stone-400 group-hover:text-white">#001</span>
+                            </div>
+                            <div :class="howOrgPoppup==true?'flex-col mt-6 w-full':'flex-row'" class="flex  justify-between">
+                                <div class=" flex flex-row ">
+                                    <div class=" flex flex-row mx-1 ">
+                                        <btn_Vue class=" " :options="{label:item.label_btn1,style:' bg-teal-700 py-1 text-stone-100 '}"></btn_Vue>
+                                    </div>
+                                    <div class=" flex flex-row mx-1 ">
+                                        <btn_Vue class=" " :options="{label:item.label_btn2,style:'bg-teal-700 py-1 text-stone-100 '}"></btn_Vue>
+                                    </div>
+                                    <div class=" flex flex-row ">
+                                        <btn_Vue v-if="howOrgPoppup!=true" class=" shadow-md " :options="{label:item.label_btn4,style:' bg-stone-200 py-1 text-red ',ico:$store.state.icons.delete}"></btn_Vue>
+
+                                    </div>
+                                </div>
+                                <div class=" bg-transparent flex flex-row mx-1 w-full mt-4 ">
+                                    <div class="  w-5/6 items-center justify-center flex text-center bg-white  rounded-md ">
+                                        <btn_Vue class=" " :options="{label:item.label_btn3,style:' bg-stone-100 py-1 text-stone-800 '}"></btn_Vue>
+                                    </div>
+                                    <button v-if="howOrgPoppup==true" class=" ml-3 bg-white p-1 px-2 rounded-md">
+                                        <svg class=" w-4 fill-current text-red-500" viewBox="0 0 24 24">
+                                            <path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12z" /></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div v-else class="text-xs  text-stone-500 flex-col w-full h-full items-center justify-center flex">
+                    <img src="../assets/vide.svg" alt="">
+                    <flex class=" flex items-center my-3 flex-col">
+                        <span>Vous avez aucun formation.</span>
+                        <span>Cliquer ici pour creer.</span>
+                    </flex>
+                    <div class=" flex flex-row mx-1  ">
+                        <btn_Vue class=" " :options="{label:'Nouvelle',ico:$store.state.icons.plus,style:' base_bg text-white py-2 text-stone-800 '}"></btn_Vue>
+                    </div>
+                </div>
+            </div>
+            <div class=" sticky flex flex-col  mx-3 bg-white rounded-lg w-full px-5">
+                <div class=" z-20 sticky top-12 mx-3 bg-white rounded-lg py-5 px-5 flex justify-between w-full">
+                    <span class=" font-semibold">Formulaire de base</span>
+                    <div class="flex items-center flex-row text-sm ">
+                        <span v-for="item,i in listeMenu" @click="()=>{indexFormulaire=i}" :key="i" :class="indexFormulaire==i?' bg-teal-500 text-white rounded-md':''"  class=" mx-2 py-1 px-2" v-text="item"></span> 
+
+                    </div>
+                </div>
+                <actionFormationVue v-if="indexFormulaire==0"></actionFormationVue>
             </div>
         </div>
     </div>
@@ -55,50 +81,52 @@
 </div>
 </template>
 
-  
-  
 <script>
 import btn_Vue from '../components/button/btn_.vue'
+import popup from '../components/poppup/organisme.vue'
+import actionFormationVue from '../components/poppup/actionFormation.vue'
 export default {
     components: {
         btn_Vue,
+        popup,
+        actionFormationVue
     },
     data() {
         return {
-            listeFormatoin: [
-                //     {
-                //     titre: 'Titre formation',
-                //     label_btn1: 'Gestion bénéficiaire',
-                //     label_btn2: 'Gestion session',
-                //     label_btn3: 'Gestion Formation',
-                //     label_btn4: 'Supprimer',
-                // }, {
-                //     titre: 'Titre formation',
-                //     label_btn1: 'Gestion bénéficiaire',
-                //     label_btn2: 'Gestion session',
-                //     label_btn3: 'Gestion Formation',
-                //     label_btn4: 'Supprimer',
-                // }, {
-                //     titre: 'Titre formation',
-                //     label_btn1: 'Gestion bénéficiaire',
-                //     label_btn2: 'Gestion session',
-                //     label_btn3: 'Gestion Formation',
-                //     label_btn4: 'Supprimer',
-                // }, {
-                //     titre: 'Titre formation',
-                //     label_btn1: 'Gestion bénéficiaire',
-                //     label_btn2: 'Gestion session',
-                //     label_btn3: 'Gestion Formation',
-                //     label_btn4: 'Supprimer',
-                // },
-            ]
+            listeFormatoin: [{
+                titre: 'Titre formation',
+                label_btn1: 'Gestion bénéficiaire',
+                label_btn2: 'Gestion session',
+                label_btn3: 'Gestion Formation',
+                label_btn4: 'Supprimer',
+            }, {
+                titre: 'Titre formation',
+                label_btn1: 'Gestion bénéficiaire',
+                label_btn2: 'Gestion session',
+                label_btn3: 'Gestion Formation',
+                label_btn4: 'Supprimer',
+            }, {
+                titre: 'Titre formation',
+                label_btn1: 'Gestion bénéficiaire',
+                label_btn2: 'Gestion session',
+                label_btn3: 'Gestion Formation',
+                label_btn4: 'Supprimer',
+            }, {
+                titre: 'Titre formation',
+                label_btn1: 'Gestion bénéficiaire',
+                label_btn2: 'Gestion session',
+                label_btn3: 'Gestion Formation',
+                label_btn4: 'Supprimer',
+            }, ],
+            listeMenu:['ActionFormation','Bilan','VAE'],
+            howOrgPoppup: true,
+            indexFormulaire:0,
         }
     }
 
 }
 </script>
-  
-  
+
 <style>
     
     </style>
